1e51930093aca2617cc87ff8bc65ef88c03b51c2
Varghese Babu <vargheseakhil1512@gmail.com>
2023-10-24 22:11:25 +0200

    feat: update app.js
    updating the app file from GitHub

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const http = require('http')
 
 
 http.createServer(function(req, res){
-
-
-	res.write("On the way to become full stack!!")
+	res.write("On the way to become full stack!! Just staretd")
 	res.end()
-
 }
 ).listen(3000)
 
